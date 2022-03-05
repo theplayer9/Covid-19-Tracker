e056d99156786a0151954ba1392f0d178bc6e552
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-05 16:42:08 +0530

    dummy dropdown ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react"
-import './App.css';
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([
+    "usa",
+    "india",
+    "australia",
+    "japan",
+  ]);
+
   return (
-    <div className="App">
-      <h1>Let's build covid tracker</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="avs">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+
+            {/* hard coded */}
+            {/* <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">2</MenuItem>
+            <MenuItem value="worldwide">3</MenuItem>
+            <MenuItem value="worldwide">4</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
