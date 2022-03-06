aa82429583791084150a135631eca107ac1883a7
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-06 23:55:52 +0530

    added worldwide option also

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="avs">
+          <MenuItem value="worldwide">worldwide</MenuItem>
             {countries.map((country) => {
               return <MenuItem value={country.value}>{country.name}</MenuItem>; //this is the ES6 syntax.
             })}
