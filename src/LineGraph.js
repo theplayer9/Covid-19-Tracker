90007a37b27c52b2e15f4e45c89c372b45c8938c
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-08 19:47:12 +0530

    some change

---
+++ src/LineGraph.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+function LineGraph() {
+  const [data, setData] = useState({});
+ // https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("line graph data",data)
+      });
+  }, []);
+
+  const buildChartData = (data , casesType='cases')=> {
+    const chartData = []
+    let lastDataPoint;
+    data[casesType].forEach(date=> {
+      if(lastDataPoint) {
+        const newDataPoint = {
+          x: date ,
+          y: data[casesType][date] - lastDataPoint
+        }
+        chartData.push(newDataPoint)
+      }
+      lastDataPoint= data[casesType][date]
+
+    })
+    return chartData;
+
+  }
+  
+
+  return (
+    <h2>I'm the player</h2>
+    // <div>
+    //   <Line
+    //   data options  />
+    // </div>
+  );
+}
+
+export default LineGraph;
